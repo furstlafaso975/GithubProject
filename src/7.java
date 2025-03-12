0034e48f49949fd5e04763af5926ad8d22285367
furstlafaso975 <furstlafaso975@gmail.com>
2025-03-12 07:30:48 +0000

    Add 7.java

---
+++ src/7.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class RandomJavaCode {
+  public static void main(String[] args) {
+    // Generate a random number between 1 and 10
+    int randNum = new Random().nextInt(10);
+    
+    // Print the random number
+    System.out.println("Random number: " + randNum);
+  }
+}
