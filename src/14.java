db519821d32a4173a7cac0cbafeef2063d7b6c5b
furstlafaso975 <furstlafaso975@gmail.com>
2025-03-19 04:24:48 +0000

    Add 14.java

---
+++ src/14.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import java.util.Random;
+
+public class RandomCode {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int num1 = random.nextInt(10);
+        int num2 = random.nextInt(10);
+        int sum = num1 + num2;
+        System.out.println("The sum is " + sum);
+    }
+}
