68eaa007570d3b5640594877a3b32180f1db5b4a
furstlafaso975 <furstlafaso975@gmail.com>
2025-03-23 10:57:30 +0000

    Add 18.java

---
+++ src/18.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Example usage of Java
+        System.out.println("Hello, World!");
+        int age = 20;
+        if (age >= 18) {
+            System.out.println("You are old enough to vote.");
+        } else {
+            System.out.println("Sorry, I don't have the right to vote.");
+        }
+    }
+}
