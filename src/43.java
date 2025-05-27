14e71b4a59b1a17c046e349b5b4a6a9c51fb8d2f
furstlafaso975 <furstlafaso975@gmail.com>
2025-05-27 23:22:22 +0000

    Add 43.java

---
+++ src/43.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Example 1: Print "Hello, World!"
+        System.out.println("Hello, World!");
+
+        // Example 2: Print "This is a sample program in Java."
+        System.out.println("This is a sample program in Java.");
+
+        // Example 3: Print the name of the class and package
+        System.out.println("Class Name: " + HelloWorld.class.getName() +
+                            ", Package Name: " + HelloWorld.class.getPackage().getName());
+    }
+}
